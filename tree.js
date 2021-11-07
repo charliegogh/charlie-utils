43c9596bbc962896cf4904728d51c6de3206d513
charliegogh <e_rrol@163.com>
2021-11-07 20:57:53 +0800

    docs - update

--- tree.js
+++ tree.js
@@ -1,4 +1,3 @@
-import {validPassword,validTel} from "./validate";
 /* 树查找 */
 export const findTree = function (tree, func) {
     for (const data of tree) {
@@ -10,3 +9,26 @@ export const findTree = function (tree, func) {
     }
     return null
 }
+/**
+ * @param tree
+ * 扁平化
+ */
+export const flattenTree = function (tree) {
+    return tree.reduce((arr, item) => {
+        arr.concat([item], flattenTree(item.children || []))
+    }, [])
+}
+/**
+ * @param list
+ * @returns {*}
+ * 列表转树
+ */
+export const listToTree = function (list) {
+    const info = list.reduce((map, node) => (map[node.id] = node, node.children = [], map), {})
+    return list.filter(node => {
+        info[node.parentId] && info[node.parentId].children.push(node)
+        return !node.parentId
+    })
+}
+
+/* 获取所有父级 */
