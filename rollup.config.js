dbf5edf2fa8d7a010c8f46d79a3645191c46025e
SX8284 <charliegogh@163.com>
2022-04-21 23:00:21 +0800

    no message

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
-import { terser } from 'rollup-plugin-terser'
-
+import { terser } from 'rollup-plugin-terser' // 压缩js代码，包括es6代码压缩
+import commonjs from 'rollup-plugin-commonjs' // 适配cjs
 export default {
-    input: './tree2.js',
+    input: './lib/index.js',
     output: {
-        file: 'dist/tree.js',
-        format: 'umd'
+        file: 'dist/index.js',
+        format: 'cjs'
     },
     plugins: [
-        terser()
+        commonjs(),
+        // terser(),
     ]
 }
---
+++ test/index.js
@@ -0,0 +1,39 @@
+const index = [
+    {
+        name: '1',
+        id: '1',
+        children: [
+            {
+                name: '1-1',
+                id: '1-1'
+            },
+            {
+                name: '1-2',
+                id: '1-2'
+            },
+        ]
+    },
+]
+import ee from '../dist/index'
+// let rs= [],queue=index
+// while (queue.length>0){
+//     [...queue].forEach(child=>{
+//         queue.shift()
+//         rs.push(child.name)
+//         child.children && (queue.push(...child.children))
+//     })
+// }
+// console.log(rs);
+
+
+
+
+
+
+
+
+
+
+
+
+
