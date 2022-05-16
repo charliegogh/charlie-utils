cb9b57ad953e0d7882badbd72c65f5323bb2a258
SX8284 <charliegogh@163.com>
2022-05-16 14:59:11 +0800

    fix: eslint配置、数组方法、数组对象分类

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,19 @@
 import { terser } from 'rollup-plugin-terser' // 压缩js代码，包括es6代码压缩
 import commonjs from 'rollup-plugin-commonjs' // 适配cjs node 适配
-import resolve from '@rollup/plugin-node-resolve'  // 允许我们加载第三方模块
+import resolve from '@rollup/plugin-node-resolve' // 允许我们加载第三方模块
 // import { eslint } from 'rollup-plugin-eslint';
 export default {
-    input: './lib/index.js',
-    output: {
-        file: 'dist/index.js',
-        format: 'umd',
-        name:'charlieUtils'
-    },
-    plugins: [
-        commonjs(),
-        resolve(),
-        // eslint()
-        terser(),
-    ],
-    // external: ['moment']
+  input: './lib/index.js',
+  output: {
+    file: 'dist/app.js',
+    format: 'umd',
+    name: 'charlieUtils'
+  },
+  plugins: [
+    commonjs(),
+    resolve(),
+    // eslint(),
+    terser()
+  ]
+  // external: ['moment']
 }
