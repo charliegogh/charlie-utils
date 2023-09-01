94ea01d9baad1a8debf09c9b08f6eee734ef578b
charlie <charliegogh@gmail.com>
2023-09-01 17:11:08 +0800

    no message

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,23 @@
 import { terser } from 'rollup-plugin-terser' // 压缩js代码，包括es6代码压缩
 import commonjs from 'rollup-plugin-commonjs' // 适配cjs node 适配
 import resolve from '@rollup/plugin-node-resolve' // 允许加载第三方模块
+import babel from 'rollup-plugin-babel';
+
 // import { eslint } from 'rollup-plugin-eslint';
 export default {
-  input: './lib/',
+  input: './lib/useMindDisplay.js',
   output: {
     file: 'dist/app.js',
-    format: 'es',
-    name: '_'
+    format: 'iife',
+    // format: 'es',
+    // format: 'umd',
+    name: 'MindDisplay'
   },
   plugins: [
     commonjs(),
     resolve(),
     // eslint(),
-    terser()
+    // terser()
   ],
-  external: ['moment']
+  // external: ['moment']
 }
--- test/tree/data.js
+++ test/tree/data.js
@@ -1,56 +1,104 @@
 const data = [
     {
-        "id": 1,
-        "name": "Root",
-        "pid": null,
+        "id": "CJFD|CXGH202302004_h2_1",
+        "parentId": "",
+        "literatureId": "CJFD|CXGH202302004",
+        "cataTitle": "4 结语",
+        "orderNum": 60000,
+        "content": null,
+        "type": 1,
+        "fileCode": null,
+        "rootChapterId": "CJFD|CXGH202302004_h1_4",
+        "markCount": 0,
+        "tagName": null,
+        "level": 1,
+        "userId": "12558536-d429-4c11-b3a4-9c0b5e981dcc",
+        "postTime": "2023/8/11 15:40:17",
+        "paras": [],
+        "noteSigns": null,
+        "catalogHinfo": "<h1 class=\"Chapter\" data-id=\"CJFD|CXGH202302004_h1_4-H\">4 结语</h1>",
+        "sourceId": null,
+        "srcId": null,
+        "srcType": null
+    },
+    {
+        "id": "2d8f95f5-c0d4-4082-834f-d913c9092205",
+        "type": "note",
+        "text": "群的上海",
+        "level": null,
+        "layout": null,
+        "colorIndex": null,
+        "background": null,
+        "font-family": "微软雅黑,Microsoft YaHei",
+        "font-size": 16,
+        "font-weight": null,
+        "font-style": null,
+        "hyperlink": null,
+        "hyperlinkTitle": null,
+        "note": null,
+        "notedev": null,
+        "notemore": null,
+        "detail": null,
+        "expandState": null,
+        "image": "",
+        "imageSize": {
+            "width": 338,
+            "height": 238
+        },
+        "color": null,
+        "appId": "CRSP_PSMC_RELEASE",
+        "parentId": "CJFD|CXGH202302004_h2_1",
+        "quote": "群的上海",
+        "contents": null,
+        "isFreeNode": 0,
+        "nodeType": 4,
         "children": [
             {
-                "id": 2,
-                "name": "Child 1",
-                "pid": 1,
-                "children": [
-                    {
-                        "id": 3,
-                        "name": "Child 1.1",
-                        "pid": 2,
-                        "children": []
+                "data": {
+                    "id": "cv5jr48a8sg0",
+                    "type": null,
+                    "text": "分支主题",
+                    "level": null,
+                    "layout": null,
+                    "colorIndex": "1",
+                    "background": null,
+                    "font-family": "微软雅黑,Microsoft YaHei",
+                    "font-size": 16,
+                    "font-weight": null,
+                    "font-style": null,
+                    "hyperlink": null,
+                    "hyperlinkTitle": null,
+                    "note": null,
+                    "notedev": null,
+                    "notemore": null,
+                    "detail": null,
+                    "expandState": null,
+                    "image": null,
+                    "imageSize": {
+                        "width": 338,
+                        "height": 238
                     },
-                    {
-                        "id": 4,
-                        "name": "Child 1.2",
-                        "pid": 2,
-                        "children": []
-                    }
-                ]
-            },
-            {
-                "id": 5,
-                "name": "Child 2",
-                "pid": 1,
-                "children": [
-                    {
-                        "id": 6,
-                        "name": "Child 2.1",
-                        "pid": 5,
-                        "children": []
-                    }
-                ]
+                    "color": null,
+                    "appId": null,
+                    "parentId": "2d8f95f5-c0d4-4082-834f-d913c9092205",
+                    "quote": null,
+                    "contents": null,
+                    "isFreeNode": 1,
+                    "nodeType": 15
+                },
+                "children": []
             }
         ]
     }
-];
+]
 
-function removeAndKeepChildren(arr, targetId) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i].id === targetId) {
-            const removedItem = arr.splice(i, 1)[0];
-            arr.splice(i, 0, ...removedItem.children);
-            return;
-        } else {
-            removeAndKeepChildren(arr[i].children, targetId);
-        }
-    }
-}
+const tree = require('../../lib/tree')
 
-removeAndKeepChildren(data, 2);
-console.log(JSON.stringify(data,null,2));
+
+const rs = tree.listToTree(data,{
+    id: 'id',
+    children: 'children',
+    pid: 'parentId'
+})
+
+console.log(JSON.stringify(rs,null,2))
