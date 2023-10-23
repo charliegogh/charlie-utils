9e5d71f150c06ce5cec6ded45d974f628e3d4cfc
charlie <charliegogh@gmail.com>
2023-10-23 09:37:11 +0800

    Revert "no message"
    This reverts commit 454ba463c7f13be0d216946c09e6e22d007dd343.

--- rollup.config.js
+++ rollup.config.js
@@ -5,29 +5,18 @@ import babel from 'rollup-plugin-babel';
 
 // import { eslint } from 'rollup-plugin-eslint';
 export default {
-  input: './lib/index.js',
+  input: './lib/useMindDisplay.js',
   output: {
     file: 'dist/app.js',
-    // format: 'iife',
+    format: 'iife',
     // format: 'es',
-    format: 'umd',
-    name: 'xe-plugin'
+    // format: 'umd',
+    name: 'MindDisplay'
   },
   plugins: [
     commonjs(),
     resolve(),
     // eslint(),
-    babel({
-      exclude: 'node_modules/**', // 不编译node_modules下的代码
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: '> 0.25%, not dead', // 目标浏览器
-          },
-        ],
-      ],
-    }),
     terser()
   ],
   // external: ['moment']
